828ec4e4746c84992006aeb5628d0c6a3c527006
Ronny Pollmer <ronnypollmer@hotmail.com>
2024-12-29 18:04:00 +0100

    added code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let fields = [null, "circle", "cross", null, null, null, null, null, null];
+let fields = [null, "circle", "cross", "circle", null, "cross", null, null, "cross"];
 
 function init() {
   render();
@@ -15,9 +15,9 @@ function render() {
       const index = i * 3 + j;
       let symbol = "";
       if (fields[index] === "circle") {
-        symbol = "O";
+        symbol = generateCircleSVG();
       } else if (fields[index] === "cross") {
-        symbol = "X";
+        symbol = generateCrossSVG();
       }
       tableHTML += `<td>${symbol}</td>`;
     }
@@ -27,4 +27,36 @@ function render() {
 
   // Set table HTML to content div
   contentDiv.innerHTML = tableHTML;
+}
+
+function generateCircleSVG() {
+    const color = '#00B0EF';
+    const width = 70;
+    const height = 70;
+    return `<svg width="${width}" height="${height}">
+              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="1s" fill="freeze" />
+              </circle>
+            </svg>`;
+}
+
+function generateCrossSVG() {
+    const color = '#FFC000';
+    const width = 70;
+    const height = 70;
+    const svgHtml = `
+      <svg width="${width}" height="${height}">
+        <line x1="0" y1="0" x2="${width}" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="0; ${width}" dur="1s" />
+          <animate attributeName="y2" values="0; ${height}" dur="1s" />
+        </line>
+        <line x1="${width}" y1="0" x2="0" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="${width}; 0" dur="1s" />
+          <animate attributeName="y2" values="0; ${height}" dur="1s" />
+        </line>
+      </svg>
+    `;
+    return svgHtml;
 }
